cf2c3960cfee5c39f5d6c161d4e5cb01aa13ac0f
Dejitaru <dejitaru@gmail.com>
2022-04-16 02:52:41 -0500

    Now you can add or remove a domain

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # subdomain-nginx
-Bash script to create a subdomain for nginx and basic php and nginx configuration
+Bash script to create or delete a domain for nginx and basic php and nginx configuration
 ## How to use it
 make the file executable, so it can be run from a terminal
 ```sh
 chown +x subdomain.sh
 ```
-Use it passing 2 arguments, subdomain and domain like this:
+Adding a domain
 ```sh
-./subdomain.sh mysubdomain mydomain.com
+./subdomain.sh add mydomain.com
 ```
-this will create mysubdomain.mydomain.com
+Removing a domain
+```sh
+./subdomain.sh remove mydomain.com
+```
+After removing domain it will ask if you also want to remove the files(located in /var/www/mydomain.com and its own folder)
+## To use it globally
+If you want to use it globally, move the script to your $PATH folder. To find your $PATH folder execute:
+```sh
+echo $PATH
+```
--- subdomain.sh
+++ subdomain.sh
@@ -2,79 +2,101 @@
 #
 # Bash script for subdomain in nginx
 
-#Functions
-ok() { echo -e '\e[32m'$1'\e[m'; } # Green
-die() { echo -e '\e[1;31m'$1'\e[m'; exit 1; }
-
 #Variables
 NGINX_SITES_AVAILABLE='/etc/nginx/sites-available'
 NGINX_SITES_ENABLED='/etc/nginx/sites-enabled'
 WEB_PATH='/var/www'
+MY_EMAIL='ventas@silo13.com'
+
+# Functions
+ok() { echo -e '\e[32m'$1'\e[m'; } # Green
+die() { echo -e '\e[1;31m'$1'\e[m'; exit 1; }
+
+add() {
+    ok "adding  $1"
+    # Create the Nginx config file.
+    cat > $NGINX_SITES_AVAILABLE/$1 <<EOF
+    server {
+        server_name $2;
+        root        $WEB_PATH/$1/public_html;
+
+        # Logs
+        access_log $WEB_PATH/$1/logs/access.log;
+        error_log  $WEB_PATH/$1/logs/error.log;
+
+        index index.php index.html index.htm;
+        client_max_body_size 12M;
+
+        location / {
+            try_files \$uri \$uri/ /index.html;
+        }
+        location ~ \.php$ {
+            try_files \$uri =404;
+            fastcgi_split_path_info ^(.+\.php)(/.+)$;
+            fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+            fastcgi_index index.php;
+            include fastcgi.conf;
+            fastcgi_buffers 16 16k;
+            fastcgi_buffer_size 32k;
+        }
+    }
+EOF
+    # Create {public,log} directories.
+    mkdir -p $WEB_PATH/$1/{public_html,logs}
+    # Create index.html file.
+    cat > $WEB_PATH/$1/public_html/index.html <<EOF
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>$1</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>subdomain $1<h1>
+        <p>$(date +%Y)</p>
+    </body>
+    </html>
+EOF
+
+    # Change the folder permissions
+    chown -R $USER:$WEB_USER $WEB_PATH/$1
+
+    # symbolic link to enable site
+    ln -s $NGINX_SITES_AVAILABLE/$1 $NGINX_SITES_ENABLED/$1
+
+    # Restart the Nginx server to apply.
+    read -p "Restart nginx to apply changes? (y/n): " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+    then
+    /etc/init.d/nginx restart;
+    fi
+}
+
+remove() {
+    ok "removing  $1"
+    rm $NGINX_SITES_ENABLED/$1
+    rm $NGINX_SITES_AVAILABLE/$1
+
+    read -p "Remove files (y/n): " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+      rm -rf $WEB_PATH/$1/
+    fi
+
+    # Restart the Nginx server to apply.
+    read -p "Restart nginx to apply changes? (y/n): " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+    then
+    /etc/init.d/nginx restart;
+    fi
+}
 
 # Sanity check.
 [ $(id -g) != "0" ] && die "Script must be running as root."
-[ $# != "2" ] && die "Usage: $(basename $0) subDomainName mainDomainName"
-
-ok "Creating the config files for your subdomain."
-
-# Create the Nginx config file.
-cat > $NGINX_SITES_AVAILABLE/$1 <<EOF
-server {
-    server_name $1.$2;
-    root        $WEB_PATH/$1.$2/public_html;
-
-    # Logs
-    access_log $WEB_PATH/$1.$2/logs/access.log;
-    error_log  $WEB_PATH/$1.$2/logs/error.log;
-
-    index index.php index.html index.htm;
-    client_max_body_size 12M;
-
-    location / {
-        try_files $uri $uri/ /index.html;
-    }
-    location ~ \.php$ {
-        try_files $uri =404;
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/run/php/php7.4-fpm.sock;
-        fastcgi_index index.php;
-        include fastcgi.conf;
-        fastcgi_buffers 16 16k;
-        fastcgi_buffer_size 32k;
-    }
-
-}
-EOF
-
-# Create {public,log} directories.
-mkdir -p $WEB_PATH/$1.$2/{public_html,logs}
-
-# Create index.html file.
-cat > $WEB_PATH/$1.$2/public_html/index.html <<EOF
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>$1.$2</title>
-    <meta charset="utf-8" />
-</head>
-<body>
-    <h1>subdomain $1.$2<h1>
-    <p>$(date +%Y)</p>
-</body>
-</html>
-EOF
-
-# Change the folder permissions
-chown -R $USER:$WEB_USER $WEB_PATH/$1.$2
-
-# symbolic link to enable site
-ln -s $NGINX_SITES_AVAILABLE/$1.$2 $NGINX_SITES_ENABLED/$1.$2
-
-# Restart the Nginx server to apply.
-read -p "Restart nginx to apply changes? (y/n): " prompt
-if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-then
-  /etc/init.d/nginx restart;
+if [ "$1" == "add" ]; then
+ add $2
+elif [ "$1" == "remove" ]; then
+ remove $2
+else
+ die "add or remove required"
 fi
-
-ok "Subdomain created $1.$2."
+ok "completed"
